79728f9ea7b334155003b61344b410e7917b1e5e
Ariel Zusman <ariel.zusman@gmail.com>
2017-08-24 11:09:00 +0300

    first commit

---
+++ block_simplehtml.php
@@ -0,0 +1,18 @@
+<?php
+class block_simplehtml extends block_base {
+  public function init() {
+    $this->title = get_string('simplehtml', 'block_simplehtml');
+  }
+
+  public function get_content() {
+    if ($this->content !== null) {
+      return $this->content;
+    }
+
+    $this->content = new stdClass;
+    $this->content->text = 'The content of our simpleHTML block!';
+    $this->class->footer - 'Footer here...';
+
+    return $this->content;
+  }
+}
